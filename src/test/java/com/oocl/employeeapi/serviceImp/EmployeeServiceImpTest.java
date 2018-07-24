fc1b7a974b7977f28f15976bc7ae9b1e8128ce5f
Terry Teng <terry.teng@oocl.com>
2018-07-24 20:00:47 +0800

    [Terry] EmployeeServiceImpTest getAllEmployee function test done

--- src/test/java/com/oocl/employeeapi/serviceImp/EmployeeServiceImpTest.java
+++ src/test/java/com/oocl/employeeapi/serviceImp/EmployeeServiceImpTest.java
@@ -3,6 +3,8 @@ package com.oocl.employeeapi.serviceImp;
 import com.oocl.employeeapi.model.Employee;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -21,5 +23,23 @@ public class EmployeeServiceImpTest {
         Employee result = employeeServiceImp.getEmployeeById((long)1);
     // then
         assertEquals("terry", result.getName());
+    }
+
+    @Test
+    public void should_return_employee_list_when_call_getAllEmployees(){
+    // given
+        Employee employee1 = mock(Employee.class);
+        Employee employee2 = mock(Employee.class);
+        EmployeeServiceImp employeeServiceImp = new EmployeeServiceImp();
+        when(employee1.getId()).thenReturn((long)1);
+        when(employee2.getId()).thenReturn((long)2);
+        employeeServiceImp.add(employee1);
+        employeeServiceImp.add(employee2);
+
+        // when
+        List<Employee> result = employeeServiceImp.getAllEmployees();
+    // then
+        assertEquals((long)1, result.get(0).getId());
+        assertEquals((long)2, result.get(1).getId());
     }
 }
